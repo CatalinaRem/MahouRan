931f7e63e22e5df3060257388662d3c3fc552244
CatalinaRem <phatthadon_k@outlook.com>
2025-08-09 20:39:12 +0700

    Add Xcode scheme and update localization strings
    Added MahouRan.xcscheme to version control for shared build and test configurations. Updated Localizable.xcstrings with new string keys for Account, Home, Main, Member, and Notification. Added a new breakpoint in ContentView.swift at line 15.

---
+++ MahouRan/homePurple.swift
@@ -0,0 +1,18 @@
+//
+//  homePurple.swift
+//  MahouRan
+//
+//  Created by Catalina Rem on 10/8/25.
+//
+
+import SwiftUI
+
+struct homePurple: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    homePurple()
+}
